3bb01bb70a19cfe54e47b9a433b42f9fa8cb107f
Aman kumar <ag86898138@gmail.com>
2026-01-11 20:12:49 +0530

    change it

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,47 +1,26 @@
-import express from "express";
-import register, {
-  Login,
-  myProfile,
-  verifyUser,
-  enrollCourse,
-  getMyEnrolledCourses,
-  promoteToAdmin,
-  demoteFromAdmin,
-  getAllUsers,
-  createFirstAdmin,
-  updatePassword,
-  deleteAccount,
-  deleteUserByAdmin,
-  updateProfile,
-  resendOtp
-} from "../Controller/user.js";
+import express from 'express'
+import register, { Login, myProfile, verifyUser, enrollCourse, getMyEnrolledCourses, promoteToAdmin, demoteFromAdmin, getAllUsers, createFirstAdmin, updatePassword, deleteAccount, deleteUserByAdmin, updateProfile, resendOtp } from '../Controller/user.js';
+import { isAuth, isAdmin } from '../Middleware/isAuth.js';
+import { uploadFiles } from '../Middleware/multer.js';
 
-import { isAuth, isAdmin } from "../Middleware/isAuth.js";
-import { uploadFiles } from "../Middleware/multer.js";
 
-const router = express.Router();
 
-// AUTH
-router.post("/register", uploadFiles, register);
-router.post("/verify", verifyUser);
-router.post("/resend-otp", resendOtp);
-router.post("/login", Login);
-
-// USER
-router.get("/me", isAuth, myProfile);
-router.put("/update-profile", isAuth, uploadFiles, updateProfile);
-router.post("/update-password", isAuth, updatePassword);
-router.delete("/delete-account", isAuth, deleteAccount);
-
-// COURSE
-router.post("/course/:id/enroll", isAuth, enrollCourse);
-router.get("/mycourse", isAuth, getMyEnrolledCourses);
-
-// ADMIN
-router.post("/create-admin", createFirstAdmin);
-router.post("/promote", isAuth, isAdmin, promoteToAdmin);
-router.post("/demote", isAuth, isAdmin, demoteFromAdmin);
-router.get("/users", isAuth, isAdmin, getAllUsers);
-router.delete("/delete-user", isAuth, isAdmin, deleteUserByAdmin);
+const router = express.Router()
+   
+  router.post('/user/register', uploadFiles, register)
+  router.post('/user/verify', verifyUser)
+  router.post('/user/resend-otp', resendOtp)
+  router.post('/user/login', Login)
+  router.post('/user/create-admin', createFirstAdmin)
+  router.post('/user/update-password', isAuth, updatePassword)
+  router.put('/user/update-profile', isAuth, uploadFiles, updateProfile)
+  router.delete('/user/delete-account', isAuth, deleteAccount)
+  router.get("/user/me", isAuth, myProfile)
+  router.post("/course/:id/enroll", isAuth, enrollCourse)
+  router.get("/mycourse", isAuth, getMyEnrolledCourses)
+  router.post("/user/promote", isAuth, isAdmin, promoteToAdmin)
+  router.post("/user/demote", isAuth, isAdmin, demoteFromAdmin)
+  router.get("/users", isAuth, isAdmin, getAllUsers)
+  router.delete("/user/delete-user", isAuth, isAdmin, deleteUserByAdmin)
 
 export default router;
